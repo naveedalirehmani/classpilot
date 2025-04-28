9f8e956c55125027126f76d3a6ec67d379ae204e
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-04-29 02:29:52 +0500

    changed binary targets

--- server/server/dist/config/cors.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsOptions = void 0;
-exports.corsOptions = {
-    origin: [process.env.CLIENT_SERVER, process.env.CLIENT_SERVER_2],
-    credentials: true,
-    optionsSuccessStatus: 200,
-};
