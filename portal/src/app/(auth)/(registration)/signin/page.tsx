75be9f54bfd64cf5baaa75a3fea9a011c4ffac51
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-05-05 13:44:40 +0500

    added variataions

--- portal/src/app/(auth)/(registration)/signin/page.tsx
+++ portal/src/app/(auth)/(registration)/signin/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
-import LoginForm from "../components/login-form";
+import LoginForm from "../../components/login-form";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2 w-full">
-      <div className="relative hidden bg-muted lg:block">
-        <Image
-          src="/readBook.jpg"
-          alt="Image"
-          width={1000}
-          height={1000}
-          className="absolute object-cover h-full m-auto bg-muted"
-        />
+      <div className="relative hidden bg-white lg:block">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 h-3/4">
+          <Image
+            src="/signin1.png"
+            alt="Image"
+            width={1000}
+            height={1000}
+            className="object-contain"
+          />
+        </div>
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
--- portal/src/app/(auth)/(registration)/signup/page.tsx
+++ portal/src/app/(auth)/(registration)/signup/page.tsx
@@ -1,23 +1,30 @@
 import Link from "next/link";
-import Signup from "../components/signup-form";
+import Signup from "../../components/signup-form";
 import Image from "next/image";
 
 function signupForm() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2 w-full">
-      <div className="relative hidden bg-muted lg:block">
-        <Image
-          src="/readBook.jpg"
-          alt="Image"
-          width={1000}
-          height={1000}
-          className="absolute h-full object-cover bg-muted"
-        />
+      <div className="relative hidden bg-white lg:block">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 h-3/4">
+          <Image
+            src="/signup.png"
+            alt="Image"
+            width={1000}
+            height={1000}
+            className="object-contain"
+          />
+        </div>
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <Link href="/" className="flex items-center space-x-2">
-            <Image src="/mainLogo.png" alt="Logo" width={100} height={100} />
+            <Image
+              src="/mainLogo.png"
+              alt="Logo"
+              width={100}
+              height={100}
+            />
           </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
