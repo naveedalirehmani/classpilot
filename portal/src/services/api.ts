5f25c1f7ded7d01d14bf60ebf514730dd070b292
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-05-13 12:16:46 +0500

    fixed production

--- portal/src/services/api.ts
+++ portal/src/services/api.ts
@@ -3,8 +3,8 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 const URL = process.env.SERVER
 
 const instance = axios.create({
-  // baseURL: 'https://classpilot-api.kohminds.com/v1',
-  baseURL: 'http://localhost:4000/v1',
+  baseURL: 'https://classpilot-api.kohminds.com/v1',
+  // baseURL: 'http://localhost:4000/v1',
   withCredentials: true,
 });
 
